07b03cb37a5217d85de6da8a602677df2c6e6c90
Anton Kunaev <kunaevanton25@gmail.com>
2024-01-31 21:58:17 +0300

    Add custom linter config

--- main.go
+++ main.go
@@ -9,15 +9,10 @@ func main() {
 	}
 }
 
-// revive:disable:exported
-
 func PrintHello() {
 	fmt.Println("Hello, Go")
 }
 
-// revive:enable:exported
-
-// PrintNumber writes a number using the fmt.Println function
 func PrintNumber(number int) {
 	fmt.Println(number)
 }
